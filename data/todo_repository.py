3246fbc364ee9160eaeb5ecc606af59e68f3d7b6
yassinviv <159425350+YasineOuaga@users.noreply.github.com>
2025-12-16 21:49:36 +0100

    added file close and \n) for newline

--- data/todo_repository.py
+++ data/todo_repository.py
@@ -1,12 +1,13 @@
 def save_todo(todo):
     file = open("todos.txt", "a")
-    file.write(todo.text )
+    file.write(todo.text + "\n")
     file.close()
 
 def get_all_todos():
     try:
         file = open("todos.txt", "r")
         todos = file.readlines()
+        file.close()
         return todos
     except FileNotFoundError:
         return []
